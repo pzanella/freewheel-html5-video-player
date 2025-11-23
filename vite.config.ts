2671b01191371461f1371836760012c4dee5ebef
Pietro Zanella <pietrozanella22@gmail.com>
2025-11-23 11:30:01 +0100

    feat: add pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
-import path from "path";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
     mode: "production",
-    root: process.cwd(),
     base: "/freewheel-html5-video-player/",
     build: {
         minify: true,
         modulePreload: false,
         rollupOptions: {
-            input: {
-                bundle: path.resolve(__dirname, "./src/main.ts"),
-                page: path.resolve(__dirname, "./index.html"),
-            },
             output: {
                 entryFileNames: "[name].js",
                 format: "es",
